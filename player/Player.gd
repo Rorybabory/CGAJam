a231d691661140f81eb54ce0b8d6959e4fa342a2
Rorybabory <lambertrory123@gmail.com>
2024-06-04 02:20:46 -0700

    input changes

--- player/Player.gd
+++ player/Player.gd
@@ -11,7 +11,7 @@ var SPEED = 4
 var ROTATE_SPEED = 8
 var GRAVITY = -9.81
 
-
+var target_velocity = Vector2(0,0)
 
 @onready var cameraHeight = $Node3D/Camera3D.position.y
 var cameraOffset = 0.0
@@ -24,7 +24,7 @@ func _ready():
 	pass
 
 func normalized_mouse():
-	mousePos = _mouse_position
+	mousePos = get_viewport().get_mouse_position()
 
 	mousePos /= get_viewport().get_visible_rect().size
 
@@ -35,27 +35,28 @@ func normalized_mouse():
 
 
 func _input(event):
-	if event is InputEventMouseMotion and dragging:
-		_mouse_velocity = event.relative
-	if (event is InputEventMouse):
-		_mouse_position = event.position
-
-	if event.is_action_pressed("action"):
-		if (mousePos.x <= 0 or mousePos.x >= 1 or
-			mousePos.y <= 0 or mousePos.y >= 1):
-				return
-		dragging = true;
-	elif event.is_action_released("action"):
-		dragging = false;
-		_mouse_velocity = Vector2(0,0)
+	pass
+	#if event.is_action_pressed("action"):
+		#if (mousePos.x <= 0 or mousePos.x >= 1 or
+			#mousePos.y <= 0 or mousePos.y >= 1):
+				#return
+		#dragging = true;
+	#elif event.is_action_released("action"):
+		#dragging = false;
+		#_mouse_velocity = Vector2(0,0)
 
 
-func _process(delta):
-	var target_velocity = Vector3(0,0,0)
-	normalized_mouse()
+func handle_input(delta):
+	#if mouse down and inside viewport
+	if (Input.is_action_just_pressed("action") and 
+		(mousePos.x > 0 and mousePos.x < 1 and
+		 mousePos.y > 0 and mousePos.y < 1)):
+		dragging = true
+		print("started dragging at pos: " + str(mousePos) )
+	if (Input.is_action_just_released("action")):
+		dragging = false
+		
 	
-	if (not is_on_floor()):
-		velocity.y += GRAVITY
 	if (dragging):
 		var offset = -(mousePos.x-0.5);
 		#print("offset: " + str(offset))
@@ -82,11 +83,19 @@ func _process(delta):
 				target_velocity = Vector3(forward.x, 0, forward.z) * delta * (mousePos.y-0.85) * -SPEED * 1000
 				moveTimer += delta * 30
 				cameraOffset += sin(moveTimer) * delta * (mousePos.y-0.85) * 8.0
+		
+func _process(delta):
+	target_velocity = Vector3(0,0,0)
+	normalized_mouse()
+
+	if (not is_on_floor()):
+		velocity.y += GRAVITY
+	handle_input(delta)
+
 	velocity.x = lerp(velocity.x, target_velocity.x, delta * 5)
 	velocity.z = lerp(velocity.z, target_velocity.z, delta * 5)
 	
 	$Node3D/Camera3D.position.y = cameraHeight+cameraOffset
 
 	move_and_slide();
-	#print("dragging: " + str(dragging) + " | x: " + str(mousePos.x) + " y: " + str(mousePos.y) + " | x vel: " + str(_mouse_velocity.x) + " y vel: " + str(_mouse_velocity.y))
 	pass
