96fcb038480a4b0e673f329a5f4438e58f670d41
Brevillo <oliver.beebe@digipen.edu>
2024-06-05 02:48:36 -0700

    slight change to magnet script

--- player/magnet.gd
+++ player/magnet.gd
@@ -6,23 +6,23 @@ extends Node3D
 @export var arm_range_min: Vector3
 @export var arm_range_max: Vector3
 @export var power: FloatResource
-@export var input: InputService
+@export var input_service: InputService
 
 
 func _ready() -> void:
-	input.input_received.connect(on_input)
+	input_service.input_received.connect(on_input_received)
 
 
-func on_input(event: InputEvent) -> void:
+func on_input_received(event: InputEvent):
 	scroll_input(event)
-	if event is InputEventMouseMotion:
-		drag_input(event)
+	drag_input(event)
 
 
 func scroll_input(event: InputEvent) -> void:
+
 	var scroll_target = (
-		1 if event.is_action("scroll_up") 
-		else -1 if event.is_action("scroll_down") 
+		1 if event.is_action("scroll_up")
+		else -1 if event.is_action("scroll_down")
 		else 0
 	)
 
@@ -37,17 +37,18 @@ func scroll_input(event: InputEvent) -> void:
 		power.value = move_toward(power.value, scroll_target, power_per_scroll)
 
 
-func drag_input(event: InputEventMouseMotion) -> void:
+func drag_input(event: InputEvent):
 
 	if not event is InputEventMouseMotion or not Input.is_action_pressed("magnet_drag"):
 		return
 
-	var delta =  Vector2(event.relative.x, -event.relative.y) * mouse_delta_power
+	var _mouse_delta = event.relative
+	var delta =  Vector2(_mouse_delta.x, -_mouse_delta.y) * mouse_delta_power
 
 	var target = Vector2(
 		arm_range_min.x if delta.x < 0 else arm_range_max.x,
 		arm_range_min.y if delta.y < 0 else arm_range_max.y
-		)
+	)
 
 	position.x = move_toward(position.x, target.x, abs(delta.x))
 	position.y = move_toward(position.y, target.y, abs(delta.y))
