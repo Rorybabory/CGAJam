3289072cce9d37e7df2db590342782a9bc2a9431
Rorybabory <lambertrory123@gmail.com>
2024-06-05 20:54:33 -0700

    magnet changes

--- items/item.gd
+++ items/item.gd
@@ -3,8 +3,15 @@ extends Magnetizable
 
 const snap_distance: float = 1
 const magnetized_damping: float = 10
-const magnet_force: float = 200
+const magnet_force: float = 3
 
+var magnet_velocity : Vector3 = Vector3(0,0,0)
+
+
+func _integrate_forces(state):
+	linear_velocity += magnet_velocity
+	#linear_velocity += state.total_gravity
+	
 
 func _magnet_physics_process(power: float, position: Vector3, direction: Vector3) -> void:
 
@@ -13,12 +20,8 @@ func _magnet_physics_process(power: float, position: Vector3, direction: Vector3
 	if (power > 0):
 		to_magnet = direction
 	linear_damp = abs(power) * magnetized_damping
-
-	apply_force(to_magnet * power * magnet_force, position)
-
-
+	magnet_velocity = to_magnet * power * magnet_force
 func _magnet_process(power: float, position: Vector3) -> void:
-
 	var to_magnet: Vector3 = global_position - position
 
 	freeze = to_magnet.length() < snap_distance and power < 0
