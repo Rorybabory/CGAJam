6b660ff26a290670bb3c10a9346d1c15a4a90f25
Rorybabory <lambertrory123@gmail.com>
2024-06-10 14:27:34 -0700

    Update Player.gd

--- player/Player.gd
+++ player/Player.gd
@@ -112,14 +112,13 @@ func _process(delta):
 	velocity.z = lerp(velocity.z, target_velocity.z, delta * 5)
 	
 	$CameraPivot/Camera3D.position.y = cameraHeight+cameraOffset
-<<<<<<< HEAD
 	if (velocity.length() < 2):
 		cameraOffset = lerpf(cameraOffset, 0.0, delta * 10.0)
-=======
+
 	
 	var tilt_percent = $"CameraPivot/Magnet Arm".vertical_percent
 	$CameraPivot.rotation_degrees.x = VERTICAL_TILT_RANGE * (tilt_percent - 0.5)
 
->>>>>>> 43edce1a5fc6374678f0ff80910bb0d5a37649e9
+
 	move_and_slide();
 	pass
