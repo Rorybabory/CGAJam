073a81873227713807c2751723eb0841fc80342a
Brevillo <oliver.beebe@digipen.edu>
2024-06-15 13:56:22 -0700

    created basic main menu

---
+++ main menu/main_menu.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+@export var level_scene_parent: Node
+
+@export var start_scene: PackedScene
+
+
+func _on_start_pressed() -> void:
+	var scene_instance = start_scene.instantiate()
+	level_scene_parent.add_child(scene_instance)
+	queue_free()
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
