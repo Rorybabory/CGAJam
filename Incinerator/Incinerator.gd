9a8f56e4830500216b59353ba43d0ce0bfbfed81
Rorybabory <lambertrory123@gmail.com>
2024-06-15 01:59:23 -0700

    incinerator improvements

--- Incinerator/Incinerator.gd
+++ Incinerator/Incinerator.gd
@@ -8,11 +8,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$Light.light_energy = lerpf($Light.light_energy, 15, delta * 8)
 	pass
 
 
 func _on_trigger_body_entered(body):
-	print("Entered Incinerator")
 	if (body.is_in_group("Garbage")):
+		$Explosion.pitch_scale = randf_range(0.6, 1.2)
+		$Explosion.play()
 		body.queue_free()
+		$Light.light_energy = 180
 	pass # Replace with function body.
