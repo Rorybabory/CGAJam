6c51ccc601b46135777a315b735a5a5f81b79b30
Rorybabory <lambertrory123@gmail.com>
2024-06-15 01:23:45 -0700

    incinerator

---
+++ ui/GarbageCount.gd
@@ -0,0 +1,13 @@
+extends RichTextLabel
+
+@onready var startGarbage : int = get_tree().get_nodes_in_group("Garbage").size()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var garbage = get_tree().get_nodes_in_group("Garbage").size()
+	text = "TRASH: " + str(garbage) + "/" + str(startGarbage)
+	pass
